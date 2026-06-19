207f38b831b659024eec2d5244b77f625dfadad0
Ron <mateorusconi@gmail.com>
2026-06-19 15:34:55 -0300

    desarrollo hasta el primer endpoint doctorscontroller

---
+++ Dsw2026Ej15.Api/Controllers/DoctorsController.cs
@@ -0,0 +1,33 @@
+using Dsw2026Ej15.Api.Models;
+using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Data;
+using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Controllers;
+
+[ApiController]
+[Route("/api/doctors")]
+public class DoctorsController : ControllerBase
+{
+    private readonly IPersistence _persistence;
+    public DoctorsController(IPersistence persistence)
+    {
+        _persistence = persistence;
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateDoctor([FromBody]DoctorModel.Request request)
+    {
+        if(string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.LicenseNumber))
+        {
+            return BadRequest("Nombre y matricula son requeridos.");
+        }
+        var speciality = _persistence.GetSpecialityById(request.SpecialityId);
+        if (speciality == null)
+        {
+            return BadRequest("La especialidad no existe");
+        }
+        return Created();
+    }
+
+}
---
+++ Dsw2026Ej15.Api/Models/DoctorModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Api.Models;
+
+public record DoctorModel
+{
+    public record Request(string Name, string LicenseNumber,Guid SpecialityId);
+
+}
