b2c4349a75b0aeac1a771bb154ed1a15b9f118fb
Francisco Martorell <martorellfranciscoalejandro@gmail.com>
2026-06-19 21:28:21 -0300

    Se termino el desarollo del ejercicio agregando el response a DoctorModel y terminar el DoctorController

--- Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15.Api/Program.cs
@@ -3,17 +3,13 @@ using Dsw2026Ej15.Domain.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
 builder.Services.AddControllers();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -24,5 +20,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapHealthChecks("/health-check");
 app.Run();
